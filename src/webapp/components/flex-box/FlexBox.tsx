b569fbb1f702ab44127fd89d92dc72857f908796
p3rcypj <pablo@percypj.com>
2024-05-06 00:24:35 +0000

    FlexBox CSS expose attrs

--- src/webapp/components/flex-box/FlexBox.tsx
+++ src/webapp/components/flex-box/FlexBox.tsx
@@ -1,14 +1,27 @@
-import styled from "styled-components";
+import isPropValid from "@emotion/is-prop-valid";
+import styled, { CSSProperties } from "styled-components";
 
-interface FlexBoxProps {
-    flexDirection?: string;
-    justifyContent?: string;
-    alignItems?: string;
-}
+export type FlexBoxProps = CSSProperties;
 
-export const FlexBox = styled.div<FlexBoxProps>`
+export const FlexBox = styled.div
+    .withConfig({
+        shouldForwardProp: prop => isPropValid(prop) && !CSS.supports(camelToKebabCase(prop), "unset"),
+    })
+    .attrs<FlexBoxProps>(props => ({
+        flexDirection: props.flexDirection ?? "row",
+        justifyContent: props.justifyContent ?? "center",
+        alignItems: props.alignItems ?? "center",
+    }))<FlexBoxProps>`
     display: flex;
-    flex-direction: ${props => props.flexDirection || "row"};
-    justify-content: ${props => props.justifyContent || "center"};
-    align-items: ${props => props.alignItems || "center"};
+    ${props =>
+        props &&
+        Object.entries(props)
+            .map(([key, value]) => [camelToKebabCase(key), value])
+            .filter(([key, value]) => CSS.supports(key, value))
+            .map(([key, value]) => `${key}: ${value};`)
+            .join("")}
 `;
+
+const camelToKebabCase = (str: string): string => {
+    return str.replace(/([a-z])([A-Z])/g, "$1-$2").toLowerCase();
+};
